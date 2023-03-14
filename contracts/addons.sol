f5cf887e7b44f39bf98bb72863906da7d35d0231
henrygagnier <101131194+henrygagnier@users.noreply.github.com>
2023-03-14 09:42:08 -0400

    Update addons.sol

--- contracts/addons.sol
+++ contracts/addons.sol
@@ -1,9 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
-//WARNING: THIS DOCUMENT IS UNTESTED
+
+//This document is currently untested and awaiting a testnet phase. Please do not use it in production.
+
 interface FantomSocial {
     function locks(address) external view returns (uint256 end, uint256 value);
     function profiles(address) external view returns (address owner, string memory name, uint256 timeCreated, uint256 id);
+    function posts(uint256) external view returns (address author, string memory content, uint256 timeCreated, uint256 id, uint256 harmful);
+
 }
 
 contract AddOns {
@@ -36,35 +40,39 @@ contract AddOns {
 
     function follow(address _address) external profileExists(_address) lockedValue {
         require(msg.sender != _address);
-        require(followingIndex[msg.sender][_address].isListed != true); //no double follow
+        require(followingIndex[msg.sender][_address].isListed != true);
         followersIndex[_address][msg.sender] = Index(true,followers[_address].length);
-        followers[_address].push(msg.sender);
         followingIndex[msg.sender][_address] = Index(true, following[msg.sender].length);
+        followers[_address].push(msg.sender);
         following[msg.sender].push(_address);
     }
 
     function unfollow(address _address) external profileExists(_address) lockedValue {
-        require(msg.sender != _address);
         require(followingIndex[msg.sender][_address].isListed == true);
-        delete followers[_address][followingIndex[_address][msg.sender].index];
         followersIndex[_address][msg.sender].isListed = false;
-        delete following[msg.sender][followingIndex[msg.sender][_address].index];
         followingIndex[msg.sender][_address].isListed = false;
+        delete followers[_address][followingIndex[_address][msg.sender].index];
+        delete following[msg.sender][followingIndex[msg.sender][_address].index];
     }
 
-    mapping(uint256 /*post id*/ => address[]) public likes;
+    mapping(uint256 => address[]) public likes;
     mapping(uint256 => mapping(address => Index)) internal likesIndex;
 
     //Add post exists modifier to governance reporting too (if not added)
     modifier postExists(uint256 postId) {
-        //Finish writing modifier
+        (address author, string memory content, uint256 timeCreated, uint256 id, uint256 harmful) = FantomSocial(addr).posts(postId);
+        require(author != address(0x0));
         _;
     }
 
     function like(uint256 postId) postExists(postId) public {
-        //Write
+        require(likesIndex[postId][msg.sender].isListed == false);
+        likesIndex[postId][msg.sender] = Index(true, likes[postId].length);
+        likes[postId].push(msg.sender);
     }
     function unlike(uint256 postId) postExists(postId) public {
-        //Write
+        require(likesIndex[postId][msg.sender].isListed == true);
+        likesIndex[postId][msg.sender].isListed == false;
+        delete likes[postId][likesIndex[postId][msg.sender].index];
     }
 }
